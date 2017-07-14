2b7936389f5e0a9803f58e66e132af311b7297de
Nadia Dillon <dillon.nadia@gmail.com>
2017-07-14 18:20:40 +0100

    feat: add connection listener to start scan method

--- AirPlay.swift
+++ AirPlay.swift
@@ -7,19 +7,25 @@ import MediaPlayer
 class AirPlay: RCTEventEmitter {
 
   @objc func startScan() {
-    print("init Airplay");
-    let currentRoute = AVAudioSession.sharedInstance().currentRoute
-    var isAvailable = false
-    if(currentRoute.outputs.count > 0) {
-      isAvailable = true
-      NotificationCenter.default.addObserver(
-        self,
-        selector: #selector(AirPlay.airplayChanged(sender:)),
-        name: NSNotification.Name.AVAudioSessionRouteChange,
-        object: AVAudioSession.sharedInstance())
+      print("init Airplay");
+      let currentRoute = AVAudioSession.sharedInstance().currentRoute
+      print("currentRoute", currentRoute)
+      var isAvailable = false
+      if(currentRoute.outputs.count > 0) {
+        isAvailable = true
+        for output in currentRoute.outputs {
+          if output.portType == AVAudioSessionPortBluetoothA2DP {
+            self.sendEvent(withName: "airplayConnected", body: ["connected": true])
+          }
+        }
+        NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(AirPlay.airplayChanged(sender:)),
+          name: NSNotification.Name.AVAudioSessionRouteChange,
+          object: AVAudioSession.sharedInstance())
+      }
+      self.sendEvent(withName: "airplayAvailable", body: ["available": isAvailable])
     }
-    self.sendEvent(withName: "airplayAvailable", body: ["available": isAvailable])
-  }
 
   func airplayChanged(sender: NSNotification) {
     let currentRoute = AVAudioSession.sharedInstance().currentRoute
@@ -59,10 +65,8 @@ class AirPlay: RCTEventEmitter {
 @objc(AirPlayButton)
 class AirPlayButton: RCTViewManager {
   override func view() -> UIView! {
-    let wrapperView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-    wrapperView.backgroundColor = UIColor.red
+    let wrapperView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
     wrapperView.translatesAutoresizingMaskIntoConstraints = false
-
     let volumneView = MPVolumeView(frame: wrapperView.bounds)
     volumneView.showsVolumeSlider = false
     wrapperView.addSubview(volumneView)
