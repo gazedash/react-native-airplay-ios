e9b107227497e88ab31bade40005c861dc06984d
Nadia Dillon <dillon.nadia@gmail.com>
2017-07-21 11:31:10 +0100

    fix: MPVolumeView init

--- ios/RNAirPlayManager.m
+++ ios/RNAirPlayManager.m
@@ -14,7 +14,7 @@ RCT_EXPORT_MODULE();
 
 - (UIView *)view {
 
-    MPVolumeView *volumeView = [ MPVolumeView alloc ];
+    MPVolumeView *volumeView = [[MPVolumeView alloc] init];
     volumeView.showsVolumeSlider = false;
 
     return volumeView;
