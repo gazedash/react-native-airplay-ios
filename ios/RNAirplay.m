54e59ea57b8269a6eb554d8cb39a43f529a6ec48
Nadia Dillon <dillon.nadia@gmail.com>
2017-07-25 14:07:14 +0100

    feat: add Availability queue to startScan method

--- ios/RNAirplay.m
+++ ios/RNAirplay.m
@@ -19,8 +19,9 @@ RCT_EXPORT_METHOD(startScan)
     printf("init Airplay");
     AVAudioSessionRouteDescription* currentRoute = [[AVAudioSession sharedInstance] currentRoute];
     BOOL isAvailable = false;
-    NSUInteger routeNum = [[currentRoute outputs] count];
-    if(routeNum > 0) {
+    int routeCount = (int)[[currentRoute outputs] count];
+    BOOL isSpeaker = ([[[[currentRoute outputs] objectAtIndex:0] portName] isEqualToString:@"Speaker"]);
+    if(routeCount > 0 && !isSpeaker) {
         isAvailable = true;
         BOOL isConnected = true;
         for (AVAudioSessionPortDescription * output in currentRoute.outputs) {
@@ -33,10 +34,12 @@ RCT_EXPORT_METHOD(startScan)
          selector: @selector(airplayChanged:)
          name:AVAudioSessionRouteChangeNotification
          object:[AVAudioSession sharedInstance]];
-
     }
-
+    
     [self sendEventWithName:@"airplayAvailable" body:@{@"available": @(isAvailable)}];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self isAvailable];
+    });
 }
 
 RCT_EXPORT_METHOD(disconnect)
@@ -58,6 +61,20 @@ RCT_EXPORT_METHOD(disconnect)
         }
     }
     [self sendEventWithName:@"airplayConnected" body:@{@"connected": @(isAirPlayPlaying)}];
+}
+
+
+- (void) isAvailable;
+{
+    printf("init Available");
+    AVAudioSessionRouteDescription* currentRoute = [[AVAudioSession sharedInstance] currentRoute];
+    BOOL isAvailable = false;
+    int routeCount = (int)[[currentRoute outputs] count];
+    BOOL isSpeaker = ([[[[currentRoute outputs] objectAtIndex:0] portName] isEqualToString:@"Speaker"]);
+    if(routeCount > 0 &&!isSpeaker) {
+        isAvailable = true;
+    }
+    [self sendEventWithName:@"airplayAvailable" body:@{@"available": @(isAvailable)}];
 }
 
 - (NSArray<NSString *> *)supportedEvents {
