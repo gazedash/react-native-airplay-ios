665ede784e0f21cd8c489f8760e15c51b8209f6b
Andrey <andrey@Andreys-MacBook-Pro.local>
2019-02-15 12:20:31 +0300

    Add descriptive comments to RNAirplay

--- ios/RNAirplay.m
+++ ios/RNAirplay.m
@@ -15,22 +15,28 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(startScan)
         {
-        AVAudioSessionRouteDescription* currentRoute =[[AVAudioSession sharedInstance] currentRoute];
-        int routeCount = (int)[[currentRoute outputs] count];
         NSString* deviceName;
         NSString* portType;
+        // Get current audio output
+        AVAudioSessionRouteDescription* currentRoute =[[AVAudioSession sharedInstance] currentRoute];
+        // There *might* be several audio outputs, altough I wasn't able to test it
+        // With my setups e.g. headphones, bluetooth devices, Apple TV - there is only one device at a time
         for (AVAudioSessionPortDescription * output in currentRoute.outputs) {
             deviceName = output.portName;
             portType = output.portType;
             [self sendEventWithName:@"deviceConnected" body:@{@"deviceName": deviceName, @"portType": portType}];
         }
 
+        // Add observer which will call "deviceChanged" method when audio outpout changes
+        // e.g. headphones connect / disconnect
         [[NSNotificationCenter defaultCenter]
         addObserver:self
         selector: @selector(deviceChanged:)
         name:AVAudioSessionRouteChangeNotification
         object:[AVAudioSession sharedInstance]];
 
+        // also call getConnectedDevice method immediately to send currently connected device
+        // at the time of startScan
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             [self getConnectedDevice];
         });
@@ -47,8 +53,11 @@ RCT_EXPORT_METHOD(disconnect)
     NSString *deviceName;
     NSString *portType;
     for (AVAudioSessionPortDescription *output in currentRoute.outputs) {
+        // deviceName: e.g. "My bluetooth headphones"
         deviceName = output.portName;
+        // portType: the type of connection, e.g. "BluetoothA2DPOutput", "Speaker" or "AirPlay"
         portType = output.portType;
+        // send an event to React Native listener
         [self sendEventWithName:@"deviceConnected" body:@{@"deviceName": deviceName, @"portType": portType}];
     }
 }
@@ -59,8 +68,11 @@ RCT_EXPORT_METHOD(disconnect)
     NSString *deviceName;
     NSString *portType;
     for (AVAudioSessionPortDescription *output in currentRoute.outputs) {
+        // deviceName: e.g. "My bluetooth headphones"
         deviceName = output.portName;
+        // portType: the type of connection, e.g. "BluetoothA2DPOutput", "Speaker" or "AirPlay"
         portType = output.portType;
+        // send an event to React Native listener
         [self sendEventWithName:@"deviceConnected" body:@{@"deviceName": deviceName, @"portType": portType}];
     }
 }
