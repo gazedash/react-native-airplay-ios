ccf717e63de1e898dd88cba5202fecafe8dcd542
Andrey <andrey@Andreys-MacBook-Pro.local>
2019-02-19 13:49:00 +0300

    Fix formatting in RNAirView.swift

--- ios/RNAirView.swift
+++ ios/RNAirView.swift
@@ -15,23 +15,23 @@ import MediaPlayer
         let disabled = source["disabled"]
 
         if let vv = self._volumeView {
-        if let normalImage = self.getImage(source: normal) {
-            vv.setRouteButtonImage(normalImage, for: .normal)
-        }
-        if let highlightedImage = self.getImage(source: highlighted) {
-            vv.setRouteButtonImage(highlightedImage, for: .highlighted)
-        }
-        if let selectedImage = self.getImage(source: selected) {
-            vv.setRouteButtonImage(selectedImage, for: .selected)
-        }
-        if let focusedImage = self.getImage(source: focused) {
-            if #available(iOS 9.0, *) {
-                vv.setRouteButtonImage(focusedImage, for: .focused)
+            if let normalImage = self.getImage(source: normal) {
+                vv.setRouteButtonImage(normalImage, for: .normal)
             }
-        }
-        if let disabledImage = self.getImage(source: disabled) {
-            vv.setRouteButtonImage(disabledImage, for: .disabled)
-        }
+            if let highlightedImage = self.getImage(source: highlighted) {
+                vv.setRouteButtonImage(highlightedImage, for: .highlighted)
+            }
+            if let selectedImage = self.getImage(source: selected) {
+                vv.setRouteButtonImage(selectedImage, for: .selected)
+            }
+            if let focusedImage = self.getImage(source: focused) {
+                if #available(iOS 9.0, *) {
+                    vv.setRouteButtonImage(focusedImage, for: .focused)
+                }
+            }
+            if let disabledImage = self.getImage(source: disabled) {
+                vv.setRouteButtonImage(disabledImage, for: .disabled)
+            }
         }
     }
 
@@ -44,7 +44,7 @@ import MediaPlayer
         return UIImage(data:data)
     }
 
-        override func layoutSubviews() {
+    override func layoutSubviews() {
         super.layoutSubviews()
 
         if _volumeView == nil {
@@ -65,5 +65,7 @@ import MediaPlayer
      override init(frame: CGRect) {
         super.init(frame: frame)
     }
+
      required init?(coder aDecoder: NSCoder) { fatalError("nope") }
+     
 }
